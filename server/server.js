4ed5371cf2c6d5f101df24219f1b54ddcb00b144
HerrWolf <webmasterp2m@gmail.com>
2019-03-08 18:48:49 -0800

    Login normal seccion 10

--- server/server.js
+++ server/server.js
@@ -6,19 +6,17 @@ const mongoose = require('mongoose');
 const app = express();
 
 const bodyParser = require('body-parser');
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false, }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-// app.get('/', function(req, res) {
-//     res.json('Hello World');
-// });
+//Configuracion global de rutas
+app.use(require('./routes/index'));
 
-app.use(require('./routes/usuario'));
-
-mongoose.connect(process.env.URLDB, (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
 
     if (err) throw err;
 
